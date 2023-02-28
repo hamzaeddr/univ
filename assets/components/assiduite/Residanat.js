4a61df8c31ff5a408270ed83497b2557c2e8b7c8
hamzaeddr <hamzadour06@gmail.com>
2023-02-28 15:41:41 +0100

    soufiane

--- assets/components/assiduite/Residanat.js
+++ assets/components/assiduite/Residanat.js
@@ -1,41 +1,53 @@
-
-$("#E_residanat").on("change", function () {
-  var Residanat = $('#E_residanat option:selected').val();;
-  $.ajax({
-    type: "POST",
-    url: "/api/residanat-etud/" + Residanat,
-    success: function (html) {
-      $("#P_residanat_etudiant").html(html);
-      },
-        error:function(){
-          $(".loader2").hide();
-
-           Toast.fire({
-             icon: 'error',   
-             title: 'Probleme  !',
-              });
-      }
-  });
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
 });
+$(document).ready(function () {
+$("#E_residanat").on("change", function () {
+var Residanat = $('#E_residanat option:selected').val();;
+$.ajax({
+  type: "POST",
+  url: "/api/residanat-etud/" + Residanat,
+  success: function (html) {
+    $("#P_residanat_etudiant").html(html).select2();
+  //   console.log(html);
+  //   $("#P_residanat_etudiant").prop("selectedIndex", 1);
+    },
+      error:function(){
+        $(".loader2").hide();
 
+         Toast.fire({
+           icon: 'error',   
+           title: 'Probleme  !',
+            });
+    }
+});
+});
+$('select').select2();
 $("body #create_extractionRESIDANT").on("click", function () {
-  var hd = $("#datetimeDsituation").val();
-  var hf = $("#datetimeFsituation").val();
-  var choixresidant = $('#E_residanat option:selected').val();
+var hd = $("#datetimeDsituation").val();
+var hf = $("#datetimeFsituation").val();
+alert(hf);
+var choixresidant = $('#P_residanat_etudiant option:selected').val();
+if($('input[name="details"]:checked'))
+{
+  window.open('assiduite/assiduites/excelyr/'+choixresidant+"/"+hd+"/"+hf, '_blank');
+}else{
+  window.open('assiduite/assiduites/excely/'+choixresidant+"/"+hd+"/"+hf, '_blank');
+}
 
-  window.open('/excely/'+element,+hd,+hf, '_blank');
 
 
 });
 // $('#E_residanat').select2();
-$('#P_residanat_etudiant').select2();
+// $('#P_residanat_etudiant').select2();
 
+});
 
-
-///////test checkbox////
-if($('input[name="details"]:checked'))
-{
-  // checked
-}else{
-// unchecked
-}
